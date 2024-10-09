cb8ab091133f9796e4ffc3c675e507a704c61bbb
yiandou <onlyian4981@gmail.com>
2024-10-09 13:16:52 -0700

    Works

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import wikipediaapi
 import time
-
+from queue import Queue
 user_agent = "p3_wiki (onlyian4981@gmail.com)"
 wiki = wikipediaapi.Wikipedia(user_agent, "en")
 
 start_page = wiki.page("Gentoo Linux")
-target_page = wiki.page("Gentoo penguin")
-#target_page = wiki.page("Ferdinand III of Castile")
+target_page = wiki.page("Bird Island, South Georgia")
 
 def get_titles(page):
     links = page.links
@@ -15,21 +14,42 @@ def get_titles(page):
         titles.append(title)
     return titles
 
-def find_path(start_page, target_page):
+def wikipedia_game_solver(start_page, target_title):
+    print(f"Starting from {start_page.title} to {target_title}")
+    start_time = time.time()
+    queue = Queue() 
     visited = set()
-    queue = [[start_page.title]]
-    while queue:
-        path = queue.pop(0)
-        page = wiki.page(path[-1])
-        if page.title == target_page.title:
-            return path
-        if page.title in visited:
-            continue
-        visited.add(page.title)
-        for title in get_titles(page):
-            new_path = list(path)
-            new_path.append(title)
-            queue.append(new_path)
-    return None
+    parent = {}
 
+    queue.put(start_page.title)
+    visited.add(start_page.title)
 
+    while not queue.empty():
+        current_page_title = queue.get()
+        if current_page_title == target_title.title:
+            break
+
+        current_page = wiki.page(current_page_title)
+        links = get_titles(current_page)
+        
+        for link in links:
+            if link not in visited:
+                queue.put(link)
+                visited.add(link)
+                parent[link] = current_page_title
+
+    path = []
+    page_title = target_page.title
+    while page_title != start_page.title:
+        path.append(page_title)
+        page_title = parent[page_title]
+
+    path.append(start_page.title)
+    path.reverse()
+
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time}")
+    return path
+
+path = wikipedia_game_solver(start_page, target_page)
+print(path)
